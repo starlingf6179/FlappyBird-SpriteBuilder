313622552aa201d8f116ceba98bb95f71d2b583d
Jess N Star Mac <jessnstarmac@Jesss-MacBook-Pro.local>
2014-11-09 15:00:10 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -4,7 +4,7 @@
 
 @implementation
 
-- (void)initialize (gamePlayScene)
+-(void)initialize
 
 {
     // your code here;
@@ -12,15 +12,16 @@
     [physicsNode addChild:character];
 }
 
-- (void)update:(CCTime)delta
+-(void)update:(CCTime)
 
-{    // put new methods here;
-
-- (void)touchBegan:(UITouch *)touchwithEvent:(UIEvent *)event {
+{ // put new methods here;}
+    
+    
+-(void)touchBegan:(UITouch *)touchwithEvent:(UIEvent *)event {
     // this will get called every time the player touches the screen
     }
 
-- (type I return)nameOfMethod: (type of first parameter) firstParameter continuationOfMethodName: (type of second parameter) secondParameter
+-(type I return)nameOfMethod: (type of first parameter) firstParameter continuationOfMethodName: (type of second parameter) secondParameter
 
 
 @end
